a78192158e1f60305cde5f477a0e97e27a6ff60c
Gerald Reyes <gerhuete@gmail.com>
2025-06-30 18:12:30 -0500

    adding config for migration

---
+++ ExpenseTracker.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using ExpenseTracker.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Infrastructure.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+
+    public virtual DbSet<ExpenseSubCategory> ExpenseSubCategories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=expensesControl;User Id=sa;Password=SqlServer1995;Encrypt=False;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ExpenseCategory>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__ExpenseC__3214EC07053432A1");
+
+            entity.ToTable("ExpenseCategory");
+
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(sysdatetime())");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.UpdatedAt).HasDefaultValueSql("(sysdatetime())");
+        });
+
+        modelBuilder.Entity<ExpenseSubCategory>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__ExpenseS__3214EC078C60D442");
+
+            entity.ToTable("ExpenseSubCategory");
+
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(sysdatetime())");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.UpdatedAt).HasDefaultValueSql("(sysdatetime())");
+
+            entity.HasOne(d => d.ParentCategory).WithMany(p => p.ExpenseSubCategories)
+                .HasForeignKey(d => d.ParentCategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ExpenseSubCategory_ExpenseCategory");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ExpenseTracker.Infrastructure/Data/Models/ExpenseCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.Infrastructure.Data.Models;
+
+public partial class ExpenseCategory
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual ICollection<ExpenseSubCategory> ExpenseSubCategories { get; set; } = new List<ExpenseSubCategory>();
+}
